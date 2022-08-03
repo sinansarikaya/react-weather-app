c16f45092f6111b86410afb038e16558b97f237e
Sinan <sinan.sarykaya@gmail.com>
2022-08-03 16:19:58 +0200

    theme edited

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -30,10 +30,14 @@ function Weather() {
     setUnit(unit == "metric" ? "imperial" : "metric");
   };
   const dt = weathers?.current?.dt;
-  const day = new Date(dt * 1000);
 
-  function createDate(dt, type) {
-    var day = new Date(dt * 1000);
+  function createDate(dt) {
+    const newDate = new Date(dt * 1000);
+    return newDate.toDateString().slice(3);
+  }
+
+  function createDay(dt, type) {
+    const day = new Date(dt * 1000);
     if (type === "long") {
       let options = {
         weekday: "long",
@@ -48,139 +52,136 @@ function Weather() {
   }
 
   return (
-    <div>
+    <>
       <aside>
-        <div className="aside-conteiner">
-          <select onChange={handleChange}>
-            {cities.map((city) => (
-              <option
-                key={city.id}
-                value={[
-                  city.id,
-                  city.name,
-                  city.latitude,
-                  city.longitude,
-                  city.population,
-                  city.region,
-                ]}
-              >
-                {city.name}
-              </option>
-            ))}
-          </select>
-          <h1 className="city-title">{selected.name}</h1>
-          {weathers?.current?.weather?.[0].icon && (
-            <img
-              src={`http://openweathermap.org/img/wn/${weathers?.current?.weather?.[0].icon}@2x.png`}
-            />
-          )}
-          <div className="aside-info">
-            <div className="info-item">
-              {weathers?.current?.weather?.[0].description}
+        <div className={`aside ${theme}`}>
+          <div className="aside-container">
+            <div className="aside-header">
+              <select onChange={handleChange}>
+                {cities.map((city) => (
+                  <option
+                    key={city.id}
+                    value={[
+                      city.id,
+                      city.name,
+                      city.latitude,
+                      city.longitude,
+                      city.population,
+                      city.region,
+                    ]}
+                  >
+                    {city.name}
+                  </option>
+                ))}
+              </select>
             </div>
-            <div className="info-item info-date">
-              <div> {day.toDateString().slice(3)}</div>
-              <div> {createDate(dt)}</div>
-            </div>
-            <div className="info-item currentTemp">
-              {Math.round(weathers?.current?.temp)}&#176;
-            </div>
-
-            <div className="info-item info-footer">
-              <div className="info-item items">
-                <div className="sub-items">
+            <div className="aside-main">
+              <h1>{selected.name}</h1>
+              <h2>
+                <span>{createDate(weathers?.current?.dt)}</span>
+                <span>{createDay(dt)}</span>
+              </h2>
+              {weathers?.current?.weather?.[0].icon && (
+                <img
+                  src={`http://openweathermap.org/img/wn/${weathers?.current?.weather?.[0].icon}@2x.png`}
+                />
+              )}
+              <span className="aside-degree">
+                {Math.round(weathers?.current?.temp)}&#176;
+              </span>
+              <div className="aside-main-item">
+                <div>
                   Feels Like
                   <span className="material-symbols-rounded">
                     device_thermostat
                   </span>
                 </div>
-                <div>{Math.round(weathers?.current?.feels_like)}&#176;</div>
+                <span>{Math.round(weathers?.current?.feels_like)}&#176;</span>
               </div>
-
-              <div className="info-item humidity items">
-                <div className="sub-items">
+              <div className="aside-main-item">
+                <div>
                   Day
                   <span className="material-symbols-rounded">light_mode</span>
                 </div>
-                <div>{Math.round(weathers?.daily?.[0]?.temp?.day)}&#176;</div>
+                <span>{Math.round(weathers?.daily?.[0]?.temp?.day)}&#176;</span>
               </div>
-
-              <div className="info-item humidity items">
-                <div className="sub-items">
+              <div className="aside-main-item">
+                <div>
                   Night
                   <span className="material-symbols-rounded">bedtime</span>
                 </div>
-                <div>{Math.round(weathers?.daily?.[0]?.temp?.night)}&#176;</div>
+                <span>
+                  {Math.round(weathers?.daily?.[0]?.temp?.night)}&#176;
+                </span>
               </div>
-
-              <div className="info-item humidity items">
-                <div className="sub-items">
+              <div className="aside-main-item">
+                <div>
                   Humidity
                   <ion-icon name="water"></ion-icon>
                 </div>
-                <div>{weathers?.current?.humidity}</div>
+                <span>{weathers?.current?.humidity}</span>
               </div>
-
-              <div className="info-item humidity items">
-                <div className="sub-items">
+              <div className="aside-main-item">
+                <div>
                   Wind
                   <span className="material-symbols-rounded">air</span>
                 </div>
-                <div>{weathers?.current?.wind_speed}</div>
+                <span>{weathers?.current?.wind_speed}</span>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className="aside-footer">
-          <span
-            className="mode"
-            onClick={() => setTheme(theme === "Dark" ? "Light" : "Dark")}
-          >
-            {theme === "Dark" ? (
-              <ion-icon name="sunny"></ion-icon>
-            ) : (
-              <ion-icon name="moon"></ion-icon>
-            )}
-          </span>
-
-          <div className="unity">
-            <div>C</div>
-            <div>
-              <label className="switch">
-                <input type="checkbox" onChange={handleSwitch} />
-                <span className="slider round"></span>
-              </label>
+            <div className="aside-footer">
+              <span
+                className="mode"
+                onClick={() => setTheme(theme === "Dark" ? "Light" : "Dark")}
+              >
+                {theme === "Dark" ? (
+                  <ion-icon name="sunny"></ion-icon>
+                ) : (
+                  <ion-icon name="moon"></ion-icon>
+                )}
+              </span>
+              <div className="unity">
+                <div>C</div>
+                <div>
+                  <label className="switch">
+                    <input type="checkbox" onChange={handleSwitch} />
+                    <span className="slider round"></span>
+                  </label>
+                </div>
+                <div>F</div>
+              </div>
+              <a
+                href="https://github.com/sinansarikaya/react-weather-app"
+                target="_blank"
+                className={`logo-github ${theme}`}
+              >
+                <ion-icon name="logo-github"></ion-icon>
+              </a>
             </div>
-            <div>F</div>
           </div>
-
-          <a
-            href="https://github.com/sinansarikaya/react-weather-app"
-            target="_blank"
-            className={`logo-github ${theme}`}
-          >
-            <ion-icon name="logo-github"></ion-icon>
-          </a>
         </div>
       </aside>
       <section>
-        {weathers?.daily?.map((dayily, i) => (
-          <div key={i} className="grid-items">
-            <img
-              src={`http://openweathermap.org/img/wn/${dayily?.weather?.[0].icon}@2x.png`}
-            />
-            <br />
-            {Math.round(dayily.temp.day)}&#176;
-            <div>{dayily?.weather?.[0]?.description}</div>
-            <div>{Math.round(dayily?.temp?.day)}&#176;</div>
-            <div className="night_degree">
-              {Math.round(dayily?.temp?.night)}&#176;
+        <div className="section-container">
+          {weathers?.daily?.map((dayily, i) => (
+            <div key={i} className={`grid-item ${theme}`}>
+              <div className="grid-item-header">{createDate(dayily?.dt)}</div>
+              <div className="grid-item-container">
+                <img
+                  src={`http://openweathermap.org/img/wn/${dayily?.weather?.[0].icon}@2x.png`}
+                />
+                <span>{createDay(dayily?.dt)}</span>
+                <span>{dayily?.weather?.[0]?.description}</span>
+              </div>
+              <div className="grid-item-footer">
+                <div>Min: {Math.round(dayily?.temp?.min)}&#176;</div>
+                <div>Max: {Math.round(dayily?.temp?.max)}&#176;</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
-    </div>
+    </>
   );
 }
 
